d19bd3d10f1707e8ba724005534b3113667d7a8e
Robby Noyes <robertjnoyes1@gmail.com>
2021-03-17 21:21:50 -0700

    initial load page updated with functional DOM elements, base CSS

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import loadPage from './modules/initalLoad'
+
+loadPage()
---
+++ src/modules/initalLoad.js
@@ -0,0 +1,72 @@
+//Creates the header HTML, appends an h1
+function createHeader(id, text) {
+  const header = document.createElement("header");
+  header.setAttribute("id", id);
+  const logo = document.createElement("h1");
+  logo.textContent = text;
+  header.appendChild(logo);
+  return header;
+}
+
+//Creates a Tab
+function createTabs(id, text) {
+  const li = document.createElement("li");
+
+  li.setAttribute("id", id);
+
+  const a = document.createElement("a");
+  a.href = "#";
+  a.textContent = text;
+
+  li.appendChild(a);
+
+  return li;
+}
+
+function createNav(id) {
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", id);
+
+  const homeTab = createTabs("home", "Home");
+  const menuTab = createTabs("menu", "Menu");
+  const aboutTab = createTabs("about", "About");
+
+  ul.appendChild(homeTab);
+  ul.appendChild(menuTab);
+  ul.appendChild(aboutTab);
+
+  return ul;
+}
+
+function createMain(id) {
+  const main = document.createElement("main");
+  main.setAttribute("id", id);
+  return main;
+}
+
+function createFooter(id, text) {
+  const footer = document.createElement("footer");
+  footer.setAttribute("id", id);
+  const h3 = document.createElement("h3");
+  h3.textContent = text;
+  footer.appendChild(h3);
+  return footer;
+}
+
+function loadPage() {
+  const content = document.getElementById("content");
+
+  const header = createHeader("header", "Arlo");
+  content.appendChild(header);
+
+  const nav = createNav("nav");
+  header.appendChild(nav);
+
+  const main = createMain("main");
+  content.appendChild(main);
+
+  const footer = createFooter("footer", "Made by Rob");
+  content.appendChild(footer);
+}
+
+export default loadPage;
