f5ae055327a969b9306c39d277b13e8693b1e47a
Robby Noyes <robertjnoyes1@gmail.com>
2021-03-18 22:29:04 -0700

    Home page loads, responsive and working

--- src/modules/homePage.js
+++ src/modules/homePage.js
@@ -18,6 +18,14 @@ function createP(id, text) {
   return p;
 }
 
+function createLink(id, src, text){
+    const href = document.createElement('a')
+    href.setAttribute('id', id)
+    href.href = src
+    href.textContent = text
+    return href
+}
+
 function createImg(id, src){
     const img = document.createElement('img')
     img.setAttribute('id', id)
@@ -31,17 +39,24 @@ function loadHome(){
     const section = createSection('details')
     main.appendChild(section)
 
-    const heading = createHeading('arlo', 'Welcome to Arlo')
+    const heading = createHeading('arlo', 'Not Just Another Ramen Bar')
     section.appendChild(heading)
 
+    const tagline = createP('tagline', 'Tagline Goes Here!')
+    section.appendChild(tagline)
+
+    const orderLink = createLink('order', '#', 'Order Now')
+    section.appendChild(orderLink)
+
     const sectionPara = createSection('para')
-    section.appendChild(sectionPara)
+    main.appendChild(sectionPara)
 
-    const img = createImg('img1', '/dist/images/ramen.jpg')
-    sectionPara.appendChild(img)
+    // const img = createImg('img1', '/dist/images/ramen.jpg')
+    // sectionPara.appendChild(img)
 
-    const p = createP('tagline', 'Inspired by American Cuisine. Built on the principle that food tastes better in larger quantities.  For the vibrant community of Portland.')
+    const p = createP('basicInfo', 'Inspired by American Cuisine. Built on the principle that food tastes better in larger quantities.  For the vibrant community of Portland.')
     sectionPara.appendChild(p)
+
 }
 
 export default loadHome
